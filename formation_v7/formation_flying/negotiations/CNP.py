6abfcbeca45e7ca23f0952f18aa8b0a5bcfef5d0
moremans <51314534+moremans@users.noreply.github.com>
2020-10-01 11:23:57 +0200

    Update CNP.py
    Starting step 1: manager and contractor definition

--- formation_v7/formation_flying/negotiations/CNP.py
+++ formation_v7/formation_flying/negotiations/CNP.py
@@ -2,5 +2,19 @@
 # This file contains the function to do Contract Net Protocol (CNP). 
 # =============================================================================
 
-# def do_CNP(flight):
-#     # the do_CNP function takes a flight-agent object
+def do_CNP(flight):
+    # the do_CNP function takes a flight-agent object
+
+    if not flight.departure_time:
+        raise Exception("The object passed to the greedy protocol has no departure time, therefore it seems that it is not a flight.")
+# =============================================================================
+# Step 1:  determining managers and contractors
+# Semiformal:
+# For first step (not at any time point):
+# -All agents determine neighborhood (the other agents that they can see)
+# -Agents with biggest neighborhood are assigned manager
+# -Remaining agents are assigned contractor
+# =============================================================================
+
+    for agent in flight:
+
